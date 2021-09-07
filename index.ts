1d462803eea351aa7a2acc966e84fedd8ebb600a
zhangchao <529743595@qq.com>
2021-09-07 16:57:07 +0800

    init

---
+++ index.ts
@@ -0,0 +1,21 @@
+// https://www.remove.bg/zh
+
+import * as fs from 'fs';
+import axios from 'axios';
+import FormData from 'form-data'
+
+const formData = new FormData();
+formData.append('image_file', fs.createReadStream('./images/img.png'));
+
+axios({
+  method: 'post',
+  url: 'https://api.remove.bg/v1.0/removebg',
+  headers: {
+    'X-Api-Key': 'XWPSg7J45oYrk72KC67iZwU4',
+  },
+  data: formData,
+}).then((response) => {
+  console.log(response);
+  if (response.status !== 200) return console.error('Error:', response.status, response.data);
+  fs.writeFileSync("no-bg.png", response.data);
+});
